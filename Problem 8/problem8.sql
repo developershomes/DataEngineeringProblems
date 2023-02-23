4f3f1a7a899cb194eecef93267c6824642082b29
shahkalpan <118063572+shahkalpan@users.noreply.github.com>
2023-02-24 09:09:51 +1000

    Problem 8

--- Problem 8/problem8.sql
+++ Problem 8/problem8.sql
@@ -1,12 +1,28 @@
+--For Top 10 hoghest travlled users
 SELECT q.user_id, q.name, q.total
 FROM
 ( select user_id
       ,name
       , sum(distance) as total
       , RANK() OVER (ORDER BY sum(distance) DESC) as actualrank
-      from lyft_rides_log as log
-LEFT OUTER JOIN lyft_users as users
+      from DATAENG.ride_log as log
+LEFT OUTER JOIN DATAENG.user as users
 ON log.user_id = users.id
 GROUP BY user_id, name
 ORDER BY sum(distance) DESC) as q
+WHERE q.actualrank <= 10
+
+
+--For Top 10 Least travlled users
+SELECT q.user_id, q.name, q.total
+FROM
+( select user_id
+      ,name
+      , sum(distance) as total
+      , RANK() OVER (ORDER BY sum(distance)) as actualrank
+      from DATAENG.ride_log as log
+LEFT OUTER JOIN DATAENG.user as users
+ON log.user_id = users.id
+GROUP BY user_id, name
+ORDER BY sum(distance)) as q
 WHERE q.actualrank <= 10
